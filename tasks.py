ed957e7b1f5d3f0e9d8d1ef63acf5a0b5d0e0ea7
Amit Bhagat <scet.amit@gmail.com>
2015-12-17 16:16:31 -0800

    change checks to tasks

---
+++ tasks.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+from framework import CheckOperation
+from os_versions import supported_versions
+
+
+@CheckOperation()
+def get_facts(context,target):
+    facts = context.get_operation('get_facts',target)
+    print facts
+    return None
+
+@CheckOperation()
+def get_prefixes_received_from_neighbor(context,target,neighbor):
+    #print context.info
+    prefixes = context.get_operation('get_prefixes_received_from_neighbor',target,neighbor)
+    #print "Target: {}".format(target)
+    #print "Neighbor: {}".format(neighbor)
+    print prefixes 
+    return None
+
+@CheckOperation()
+def check_prefixes_received_from_neighbor(context,target,neighbor):
+    prefixes = context.get_operation('get_prefixes_received_from_neighbor',target,neighbor)
+    #print "Target: {}".format(target)
+    #print "Neighbor: {}".format(neighbor)
+    print prefixes
+    return None
+
+@CheckOperation()
+def get_lldp_neighbors(context,target):
+    '''
+    IOS: show lldp neighbors
+    JUNOS: show lldp neighbors
+
+    The functions returns a list of lists with each list-item containing remote device, local interface and remote interface.
+
+    Arguments:
+	target - target device on which the command should be executed.
+    '''
+    neighbors = context.get_operation('get_lldp_neighbors',target)
+    print neighbors
+    return None
+
